ccff838a58b2efa5cdf996795c9114c81d336d60
ngila-svg <nagilmusyoka@gmail.com>
2024-09-18 01:30:06 +0300

    added firebase authentication for the users profile

--- riggle_backend/profiles/models.py
+++ riggle_backend/profiles/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    social_media_links = models.JSONField(default=dict)  # To store social media handles
+    rate_card = models.FileField(upload_to='rate_cards/', null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username

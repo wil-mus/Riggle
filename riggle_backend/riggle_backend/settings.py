cd4b8d903101feecafceb0ca4bf39f61858fe803
ngila-svg <nagilmusyoka@gmail.com>
2024-09-18 01:17:51 +0300

    postgres setup

--- riggle_backend/riggle_backend/settings.py
+++ riggle_backend/riggle_backend/settings.py
@@ -75,10 +75,15 @@ WSGI_APPLICATION = 'riggle_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'riggle_db',
+        'USER': 'wilfred_musyoka@yahoo.com',
+        'PASSWORD': 'A9%Mrdvs',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation

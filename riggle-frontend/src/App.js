748ffb1103a54a3e652011f8b8ca82e5aa439bbd
ngila-svg <nagilmusyoka@gmail.com>
2024-09-19 01:57:19 +0300

    set up for routing using the react router

---
+++ riggle-frontend/src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import LoginPage from './pages/LoginPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;

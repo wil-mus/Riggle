7c1750c2dd906eb4e2a38659b619357381dda5db
ngila-svg <nagilmusyoka@gmail.com>
2024-09-20 01:22:08 +0300

    added a navbar component

---
+++ riggle-frontend/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/profile">Profile</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
